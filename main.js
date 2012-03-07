7b40e2b207eac6e10c1641123d5c907dbd51f82a
Cory Green <greenmachine@fullsail.edu>
2012-03-07 15:05:47 -0800

    fixed link between java and html files

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 // Author: Cory Green
 // Date: 03/06/2012
 // project: Week 2 web-App
+
+// targeting 		getElementsByTagName("tagname");	returns a collection
+// 					getElementById("idvalue");
+
+
+// when the window first loads, it will call on this addBorder function
+window.onLoad = addBorder();
+
+// creates a border around my ul elements //
+function addBorder(){
+	var getUltagname = document.getElementsByTagName("ul");
+	getUltagname[1].style.border = "10px solid black";
+}
