9110954d0271fb0129158b2a2e5ee9a81e0a319d
Cory Green <greenmachine@fullsail.edu>
2012-03-08 00:46:12 -0800

    changed my form drop down menu to be dynamic

--- main.js
+++ main.js
@@ -3,44 +3,51 @@
 // Date: 03/06/2012
 // project: Week 2 web-App
 
-// targeting 		getElementsByTagName("tagname");	returns a collection
-// 					getElementById("idvalue");
+//waiting until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
 
-// changes color of input field when you click inside it.
-function styleField1()
+function $(x)
 {
-	var field = document.getElementById("fname");
-	field.style.backgroundColor = "#0903FF";
-}
-// changes color of input field when you click inside it.
-function styleField2()
-{
-	var field = document.getElementById("bname");
-	field.style.backgroundColor = "#0903FF";
+	var theElement = document.getElementById(x);
+	return theElement;
 }
 
-// changes the color back to original color when input field looses focus.
-function unstyledField1()
-{
-	var field = document.getElementById("fname");
-	field.style.backgroundColor = "#FFFFFF";
+//create select field elements and populate with elements
+function makeCatagory(){
+	var formTag = document.getElementsByTagName("form"),
+		selectLi = $('select'),
+		makeSelect = document.createElement('select');
+		makeSelect.setAttribute("id", "groups");
+	for(var i = 0, j = bandType.length; i < j; i++)
+	{
+		var makeOption = document.createElement('option');
+		var optText = bandType[i];
+		makeOption.setAttribute("value", optText);
+		makeOption.innerHTML = optText;
+		makeSelect.appendChild(makeOption);
+	}
+	selectLi.appendChild(makeSelect);
 }
+// variable defaults
+var bandType = ["--choose a genre--","soft rock", "rock", "hard rock", "hard rock", "metal",
+				"prog rock", "country", "folk", "classical", "rap", "kids music",
+				"jazz", "other"];
+makeCatagory();
+// shows data function
+var dispInfo = $('dispInfo');
+dispInfo.addEventListener("click", getData);
 
-// changes the color back to original color when input field looses focus.
-function unstyledField2()
-{
-	var field = document.getElementById("bname");
-	field.style.backgroundColor = "#FFFFFF";
-}
+// set submit click events
+var submit = $('submit');
+submit.addEventListener("click", storeData);
 
-// when the window first loads, it will call on this addBorder function
-window.onLoad = addBorder();
+// clear function
+var clear = $('reset');
+clear.addEventListener("click", clearLocal);
 
-// creates a border around my ul elements //
-function addBorder(){
-	var getUltagname = document.getElementsByTagName("ul");
-	getUltagname[1].style.border = "10px solid black";
-}
+
+
+
+
 
 });
